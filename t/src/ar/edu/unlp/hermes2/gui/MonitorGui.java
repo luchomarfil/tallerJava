0fc65814b2a8b60e3318a732ca0018baeaa93eb6
luchomarfil <luchomarfil@gmail.com>
2015-10-21 22:36:21 -0300

    swingx

--- t/src/ar/edu/unlp/hermes2/gui/MonitorGui.java
+++ t/src/ar/edu/unlp/hermes2/gui/MonitorGui.java
@@ -1,8 +1,23 @@
 package ar.edu.unlp.hermes2.gui;
 
-public class MonitorGui {
+import javax.swing.JPanel;
+import org.jdesktop.swingx.JXDatePicker;
+import org.jdesktop.swingx.JXMonthView;
+import org.jdesktop.swingx.JXBusyLabel;
+import org.jdesktop.swingx.auth.JDBCLoginService;
+import org.jdesktop.swingx.JXErrorPane;
+
+public class MonitorGui extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8242603121100243154L;
 
 	public MonitorGui() {
+		
+		JXDatePicker datePicker = new JXDatePicker();
+		add(datePicker);
 		// TODO Auto-generated constructor stub
 	}
 
