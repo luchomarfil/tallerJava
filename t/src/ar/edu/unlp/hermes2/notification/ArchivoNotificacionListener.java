b9f56b53b3163ec2c07eaa439626617bda05b5de
luchomarfil <luchomarfil@gmail.com>
2015-11-03 16:52:50 -0300

    corregido para se pueda acceder a notificacion.txt desde eclipse y desde el jar

--- t/src/ar/edu/unlp/hermes2/notification/ArchivoNotificacionListener.java
+++ t/src/ar/edu/unlp/hermes2/notification/ArchivoNotificacionListener.java
@@ -5,7 +5,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Level;
@@ -34,10 +35,14 @@ public class ArchivoNotificacionListener implements IEventosExternosListener, Ru
 		SimpleDateFormat formatter = MonitorUtils.formatterFechaPersistencia;
 		try {
 			Thread.sleep(2000);
-			URL resource = getClass().getClassLoader().getResource("ar/edu/unlp/hermes2/resources/notificaciones.txt");
-			repositorio = new File(resource.getPath());
-			fileReader = new FileReader(repositorio);
-			BufferedReader br = new BufferedReader(fileReader);
+//							MonitorGuiPanel.class.getResource("/ar/edu/unlp/hermes2/resources/Chrysanthemum.jpg"))
+			//URL resource = ArchivoNotificacionListener.class.getResource("/ar/edu/unlp/hermes2/resources/notificaciones.txt");
+			InputStream inp = ClassLoader.getSystemClassLoader().getResourceAsStream("ar/edu/unlp/hermes2/resources/notificaciones.txt");
+			BufferedReader br = new BufferedReader(new InputStreamReader(inp));
+//			repositorio = new File(path);
+//			fileReader = new FileReader(repositorio);
+			
+//			BufferedReader br = new BufferedReader(fileReader);
 			
 			// if no more lines the readLine() returns null
 			while ((line = br.readLine()) != null) {
