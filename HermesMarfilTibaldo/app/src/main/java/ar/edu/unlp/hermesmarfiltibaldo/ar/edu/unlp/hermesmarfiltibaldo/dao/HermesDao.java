3e93ec1174f2f6b0d1ebb8c05f9e77a4dbc6adbc
Luciano <luchomarfil@gmail.com>
2015-12-14 21:35:05 -0300

    Pantalla inicial core y dao armados

---
+++ HermesMarfilTibaldo/app/src/main/java/ar/edu/unlp/hermesmarfiltibaldo/ar/edu/unlp/hermesmarfiltibaldo/dao/HermesDao.java
@@ -0,0 +1,7 @@
+package ar.edu.unlp.hermesmarfiltibaldo.ar.edu.unlp.hermesmarfiltibaldo.dao;
+
+/**
+ * Created by luciano on 14/12/15.
+ */
+public class HermesDao {
+}
---
+++ HermesMarfilTibaldo/app/src/main/java/ar/edu/unlp/hermesmarfiltibaldo/core/HermesCore.java
@@ -0,0 +1,7 @@
+package ar.edu.unlp.hermesmarfiltibaldo.core;
+
+/**
+ * Created by luciano on 14/12/15.
+ */
+public class HermesCore {
+}
