5d2582d14a9b85e53940e7ea152779007c3598b8
Luciano <luchomarfil@gmail.com>
2016-02-10 21:07:53 -0300

    App corregida, se muestran las imagenes, y las imagenes mostradas son las correctas

--- HermesMarfilTibaldo/app/src/main/java/ar/edu/unlp/hermesmarfiltibaldo/AlumnoActivity.java
+++ HermesMarfilTibaldo/app/src/main/java/ar/edu/unlp/hermesmarfiltibaldo/AlumnoActivity.java
@@ -152,7 +152,7 @@ public class AlumnoActivity extends AppCompatActivity {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
 
-            return PlaceholderFragment.newInstance(position + 1);
+            return PlaceholderFragment.newInstance(position);
         }
 
         @Override
